b6129b0956e47f27c16754d7ca3964446b6ad86e
Zach Toolson <zach.toolson@mx.com>
2017-08-30 10:00:52 -0600

    Imlement SubstringFinder class

--- spec/darryl_jenks_cr/substring_finder_spec.cr
+++ spec/darryl_jenks_cr/substring_finder_spec.cr
@@ -1,7 +1,42 @@
 require "../spec_helper"
 
 describe DarrylJenksCr::SubstringFinder do
-  it "works" do
-    true.should eq(true)
+  describe "#substring_indexes" do
+    it "returns an empty array when no occurrences are present" do
+      test_class = DarrylJenksCr::SubstringFinder.new(" ", "hello")
+      test_class.substring_indexes.should eq([] of Int32)
+    end
+
+    it "returns a single entry when word boundary on single word" do
+      test_class = DarrylJenksCr::SubstringFinder.new(/\b/, "hello")
+      test_class.substring_indexes.should eq([0])
+    end
+
+    it "returns 3 entries when word boundary on 2 words" do
+      test_class = DarrylJenksCr::SubstringFinder.new(/\b/, "hello world")
+      test_class.substring_indexes.should eq([0, 5, 6])
+    end
+
+    it "returns 2 entries when 'l' in 'hello'" do
+      test_class = DarrylJenksCr::SubstringFinder.new("l", "hello")
+      test_class.substring_indexes.should eq([2, 3])
+    end
+  end
+
+  describe "#to_a" do
+    it "returns the substring splits of word boundaries" do
+      test_class = DarrylJenksCr::SubstringFinder.new(/\b/, "hello world")
+      test_class.to_a.should eq(["hello world", " world", "world"])
+    end
+
+    it "returns an empty array when no occurrences are present" do
+      test_class = DarrylJenksCr::SubstringFinder.new(" ", "hello")
+      test_class.to_a.should eq([] of String)
+    end
+
+    it "returns 2 entires when 'l' in 'hello'" do
+      test_class = DarrylJenksCr::SubstringFinder.new("l", "hello")
+      test_class.to_a.should eq(["llo", "lo"])
+    end
   end
 end
--- src/darryl_jenks_cr/substring_finder.cr
+++ src/darryl_jenks_cr/substring_finder.cr
@@ -1,4 +1,60 @@
 module DarrylJenksCr
   class SubstringFinder
+    include Enumerable(String)
+
+    property permutable_key, source_string
+
+    def initialize(@permutable_key : (String | Regex), @source_string : String)
+    end
+
+    def each
+      results = [] of String
+
+      substring_indexes.each do |substring_index|
+        results << source_string[substring_index..-1]
+      end
+
+      results
+    end
+
+    def each(&block)
+      results = self.each
+      results.each do |result|
+        yield result
+      end
+    end
+
+    def each_substring(permutable_key)
+      @permutable_key = permutable_key
+
+      self.to_a.to_enum
+    end
+
+    def each_substring(permutable_key, &block)
+      @permutable_key = permutable_key
+
+      results = self.each
+      results.each do |result|
+        yield result
+      end
+    end
+
+    def substring_indexes
+      start_index = -1
+      string_length = source_string.size
+      substring_indexes = [] of Int32
+
+      while (start_index)
+        break if start_index >= string_length
+        substring_indexes << start_index if start_index > -1
+        start_index = source_string.index(self.permutable_key, start_index + 1)
+      end
+
+      substring_indexes
+    end
+
+    def to_a
+      self.each.to_a
+    end
   end
 end
